e3031a818a4203b0bfe50a3efe1e7850cff1363b
SpaceBa13 <brayanalpizar36@gmail.com>
2023-07-29 15:12:36 -0600

    Se agregaron archivos

--- Hola.java
+++ Hola.java
@@ -2,7 +2,7 @@ public class Hola {
     public static void main(String[] args) {
         System.out.println("Holaaaaaaaaaa");
         System.out.println(12);
-        hola_prin(5);
+        hola_prin(523423);
     }
     public static int hola_prin(int prueba){
         System.out.println(prueba);
