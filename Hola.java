4d93892009fac56c0211504c4e702578f3804fa6
SpaceBa13 <brayanalpizar36@gmail.com>
2023-07-29 15:03:21 -0600

    Se agrego funcion

--- Hola.java
+++ Hola.java
@@ -2,8 +2,14 @@ public class Hola {
     public static void main(String[] args) {
         System.out.println("Holaaaaaaaaaa");
         System.out.println(12);
+        hola_prin(2);
     }
-    public static int holaa(){
-        return 0;
+    public static int hola_prin(int prueba){
+        System.out.println(prueba);
+        return holaa(prueba);
+    }
+
+    public static int holaa(int hola){
+        return hola;
     }
 }
